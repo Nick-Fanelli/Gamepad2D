0f84dd4d1f3805bbea13023a039b657a4dee6cce
Nick <nickfanelli5@gmail.com>
2020-12-06 13:39:45 -0500

    Added in some more circle intersection methods

--- src/main/java/gamepad/physics2D/rigidbody/IntersectionDetector2D.java
+++ src/main/java/gamepad/physics2D/rigidbody/IntersectionDetector2D.java
@@ -323,4 +323,94 @@ public class IntersectionDetector2D {
 
         return true;
     }
+
+    // ============================================
+    // Circle vs. Primitive Tests
+    // ============================================
+
+    /**
+     * Returns if a circle is intersecting with a line.
+     *
+     * @param circle the circle to be checked
+     * @param line   the line to be checked
+     * @return the boolean whether or not it intersects
+     */
+    public static boolean circleAndLine(Circle circle, Line2D line) { return lineAndCircle(line, circle); }
+
+    /**
+     * Returns if a circle is intersecting with another circle.
+     *
+     * @param c1 the first circle to be checked
+     * @param c2 the second circle to be checked
+     * @return the boolean whether or not the circles intersect
+     */
+    public static boolean circleAndCircle(Circle c1, Circle c2) {
+        Vector2f vecBetweenCenters = new Vector2f(c1.getCenter()).sub(c2.getCenter());
+        float radiiSum = c1.getRadius() + c2.getRadius();
+        return vecBetweenCenters.lengthSquared() <= radiiSum * radiiSum;
+    }
+
+    /**
+     * Returns if a circle is intersecting with an AABB box.
+     *
+     * @param circle the circle to be checked
+     * @param box    the box to be checked
+     * @return the boolean whether or not the circles intersects
+     */
+    public static boolean circleAndAABB(Circle circle, AABB box) {
+        Vector2f min = box.getMin();
+        Vector2f max = box.getMax();
+
+        Vector2f closestPointToCircle = new Vector2f(circle.getCenter());
+
+        if(closestPointToCircle.x < min.x) {
+            closestPointToCircle.x = min.x;
+        } else if(closestPointToCircle.x > max.x) {
+            closestPointToCircle.x = max.x;
+        }
+
+        if(closestPointToCircle.y < min.y) {
+            closestPointToCircle.y = min.y;
+        } else if(closestPointToCircle.y > max.y) {
+            closestPointToCircle.y = max.y;
+        }
+
+        Vector2f circleToBox = new Vector2f(circle.getCenter()).sub(closestPointToCircle);
+        return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
+    }
+
+    /**
+     * Returns if a circle is intersecting with a Box2D
+     *
+     * @param circle the circle to be checked
+     * @param box    the box to be checked
+     * @return the boolean whether or not the circle intersects
+     */
+    public static boolean circleAndBox2D(Circle circle, Box2D box) {
+        // Treat the box just like an AABB, after we rotate the stuff
+        Vector2f min = new Vector2f();
+        Vector2f max = new Vector2f(box.getHalfSize()).mul(2.0f);
+
+        // Create a circle in box's local space.
+        Vector2f r = new Vector2f(circle.getCenter()).sub(box.getRigidbody2D().getPosition());
+        PhysicsMath.rotate(r, -box.getRigidbody2D().getRotation(), new Vector2f(0f, 0f));
+        Vector2f localCirclePos = new Vector2f(r).add(box.getHalfSize());
+
+        Vector2f closestPointToCircle = new Vector2f(localCirclePos);
+
+        if (closestPointToCircle.x < min.x) {
+            closestPointToCircle.x = min.x;
+        } else if (closestPointToCircle.x > max.x) {
+            closestPointToCircle.x = max.x;
+        }
+
+        if (closestPointToCircle.y < min.y) {
+            closestPointToCircle.y = min.y;
+        } else if (closestPointToCircle.y > max.y) {
+            closestPointToCircle.y = max.y;
+        }
+
+        Vector2f circleToBox = new Vector2f(localCirclePos).sub(closestPointToCircle);
+        return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
+    }
 }
